72e15bb51148df7f4e305f775e0f04faa8bd7090
braty <bora.ataysen@rollicgames.com>
2022-08-27 21:43:43 +0300

    mover unit test updated

--- Assets/Game/Scripts/Tests/EditorTests/player_movement.cs
+++ Assets/Game/Scripts/Tests/EditorTests/player_movement.cs
@@ -8,19 +8,52 @@ using UnityEngine;
 
 public class player_movement
 {
-    [Test]
-    public void moveLeft10Units_notEqualStartPosition()
+    private IPlayerController GetPlayer()
     {
-        //Arrange
         IPlayerController playerController = Substitute.For<IPlayerController>();
         GameObject gameObject = new GameObject();
         playerController.transform.Returns(gameObject.transform);
         playerController.InputReader = Substitute.For<IInputReader>();
+        return playerController;
+    }
+
+    private IMover GetMoverTranslate(IPlayerController playerController)
+    {
         IMover mover = new PlayerMovementTranslate(playerController);
-        Vector3 startPosition = playerController.transform.position;
+        return mover;
+    }
+
+    [Test]
+    public void moveLeft10Units_equalEndPosition()
+    {
+        //Arrange
+        var playerController = GetPlayer();
+        var mover = GetMoverTranslate(playerController);
 
         //Act
         playerController.InputReader.Horizontal.Returns(1f);
+        for (int i = 0; i < 10; i++)
+        {
+            mover.Tick(); // input 
+            mover.FixedTick(); // act with input
+        }
+
+        //Assert
+        Assert.AreEqual(new Vector3(10, 0, 0), playerController.transform.position);
+    }
+
+    [Test]
+    [TestCase(1f)]
+    [TestCase(-1f)]
+    public void moveHorizontal10Units_notEqualsStartPosition(float horizontalInput)
+    {
+        //Arrange
+        var playerController = GetPlayer();
+        var mover = GetMoverTranslate(playerController);
+        Vector3 startPosition = playerController.transform.position;
+
+        //Act
+        playerController.InputReader.Horizontal.Returns(horizontalInput);
         for (int i = 0; i < 10; i++)
         {
             mover.Tick(); // input 
@@ -29,28 +62,5 @@ public class player_movement
 
         //Assert
         Assert.AreNotEqual(startPosition, playerController.transform.position);
-    }
-
-
-    [Test]
-    public void moveLeft10Units_equalEndPosition()
-    {
-        //Arrange
-        IPlayerController playerController = Substitute.For<IPlayerController>();
-        GameObject gameObject = new GameObject();
-        playerController.transform.Returns(gameObject.transform);
-        playerController.InputReader = Substitute.For<IInputReader>();
-        IMover mover = new PlayerMovementTranslate(playerController);
-
-        //Act
-        playerController.InputReader.Horizontal.Returns(1f);
-        for (int i = 0; i < 10; i++)
-        {
-            mover.Tick(); // input 
-            mover.FixedTick(); // act with input
-        }
-
-        //Assert
-        Assert.AreEqual(new Vector3(10,0,0), playerController.transform.position);
     }
 }
